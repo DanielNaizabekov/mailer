6418cc0f259434dce6cd7334846da2cc4a518d9a
Daniel <danielnaizabekov@gmail.com>
2020-05-04 13:53:48 +0600

    update

--- script.js
+++ script.js
@@ -1,21 +1,16 @@
 $(document).ready(function() {
-
-	//E-mail Ajax Send
-	$("form").submit(function() { //Change
+	$("form").submit(function() {
 		var th = $(this);
-		console.log(th)
 		$.ajax({
 			type: "POST",
-			url: "mail.php", //Change
+			url: "mail.php",
 			data: th.serialize()
 		}).done(function() {
 			alert("Thank you!");
 			setTimeout(function() {
-				// Done Functions
 				th.trigger("reset");
 			}, 1000);
 		});
 		return false;
 	});
-
 });
